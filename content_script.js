02a00787cb2c3e1f2153e0ce3908fbc48769ce1d
Megh Mehta <mehtamegh@gmail.com>
2024-05-28 20:30:19 -0400

    Bug fixes

--- content_script.js
+++ content_script.js
@@ -1,12 +1,14 @@
 urls = [""];
 
-isJournal = true; //this has to be changed
+isJournal = false; //this has to be changed
 
 if (!urls.includes(window.location.hostname) && isJournal == false) {
   document.body.innerHTML = `
-    <div>
-        <h1>Website is blocked</h1> 
-        <p>Sorry, this website is blocked as you did not journal today</p>
-        <button>Journal now</button>
+    <div id="blocked-container">
+        <h1 id="website-title">Website is blocked</h1> 
+        <p id="journal-message">Sorry, this website is blocked as you did not journal today</p>
+        <p id="journal-message-controls">Use <span id="control-text">ctrl + alt + J</span> or click on the extension icon to journal now</p>
+        
     </div>`;
+  document.body.style.backgroundColor = "black";
 }
